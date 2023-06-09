43b98f38e9eb1269f4a2b6a7e05fecf79d89b461
bastxxs <bastxxs@outlook.com>
2023-06-09 17:05:26 -0300

    finalização html

--- script.js
+++ script.js
@@ -9,7 +9,30 @@ document.getElementById('newsletterForm').addEventListener('submit', function(ev
     console.log('E-mail assinado:', email);
 
     // Limpe o campo de entrada de e-mail
-    document.getElementById('emailInput').value = '';
+    document.getElementById('emailInput').value = null;
+
+
+    // Obtenha o valor do nome do input
+    var name = document.getElementById('nameInput').value;
+
+    // Envie o nome para um servidor ou faça qualquer processamento adicional
+    // Aqui, estamos apenas imprimindo o nome no console do navegador
+    console.log('Seu nome:', name);
+
+    // Limpe o campo de entrada de nome
+    document.getElementById('nameInput').value = null;
+
+    // Obtenha o valor da data do input
+    var datanasc = document.getElementById('dateInput').value;
+
+    // Envie a data para um servidor ou faça qualquer processamento adicional
+    // Aqui, estamos apenas imprimindo a data no console do navegador
+    console.log('E-mail assinado:', datanasc);
+
+    // Limpe o campo de entrada de e-mail
+    document.getElementById('dateInput').value = null;
+
+    
 
     // Exiba uma mensagem de sucesso
     alert('Obrigado por assinar a nossa newsletter!');
